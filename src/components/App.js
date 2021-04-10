30f8ed2aaa5f2e9d4d1c99772f826e71469a1c45
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-04-10 15:20:11 -0700

    add initial front end routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import Web3 from 'web3'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Web3 from 'web3';
 import './App.css';
-import Color from '../abis/Color.json'
+import Color from '../abis/Color.json';
+import Admin from '../screens/admin';
+import Login from '../screens/login';
+import Home from '../screens/home';
 
 class App extends Component {
 
@@ -88,62 +92,21 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Color Tokens
-          </a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white"><span id="account">{this.state.account}</span></small>
-            </li>
-          </ul>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <h1>Issue Token</h1>
-                <form onSubmit={(event) => {
-                  event.preventDefault()
-                  {/* ref used to keep track of input form value 
-                  this.color is defined from the ref function below*/}
-                  const color = this.color.value
-                  this.mint(color)
-                }}>
-                  <input
-                    type='text'
-                    className='form-control mb-1'
-                    placeholder='e.g. #FFFFFF'
-                    ref={(input) => { this.color = input }}
-                  />
-                  <input
-                    type='submit'
-                    className='btn btn-block btn-primary'
-                    value='MINT'
-                  />
-                </form>
-              </div>
-            </main>
-          </div>
-          <hr/>
-          <div className="row text-center">
-            { this.state.colors.map((color, key) => {
-              return(
-                <div key={key} className="col-md-3 mb-3">
-                  <div className="token" style={{ backgroundColor: color }}></div>
-                  <div>{color}</div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </div>
+     
+        <Switch>
+          <Route path="/admin" exact> \
+          <Admin
+          account={this.state.account}
+          colors={this.state.colors}
+          color={this.color}
+          mint={this.mint}
+          /> </Route>
+
+          <Route path="/login"><Login/> </Route>
+
+          <Route path="/"><Home/></Route>
+           </Switch>
+        
     );
   }
 }
---
+++ src/screens/admin.jsx
@@ -0,0 +1,65 @@
+import React, { useRef } from 'react';
+
+export default function Admin (props) {
+const inputRef = useRef(null)
+  return(
+      
+      <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Color Tokens
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-white"><span id="account">{props.account}</span></small>
+          </li>
+        </ul>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mr-auto ml-auto">
+              <h1>Issue Token</h1>
+              <form onSubmit={(event) => {
+                event.preventDefault()
+                {/* ref used to keep track of input form value 
+                this.color is defined from the ref function below*/}
+                let color = props.color.value
+                props.mint(color)
+              }}>
+                <input
+                  type='text'
+                  className='form-control mb-1'
+                  placeholder='e.g. #FFFFFF'
+                  ref={inputRef}
+                />
+                <input
+                  type='submit'
+                  className='btn btn-block btn-primary'
+                  value='MINT'
+                />
+              </form>
+            </div>
+          </main>
+        </div>
+        <hr/>
+        <div className="row text-center">
+          { props.colors.map((color, key) => {
+            return(
+              <div key={key} className="col-md-3 mb-3">
+                <div className="token" style={{ backgroundColor: color }}></div>
+                <div>{color}</div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  );
+
+}

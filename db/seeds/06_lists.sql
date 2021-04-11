3a343661ada622d9f5a0f2f2753b8978f9d8a64a
vagrant <gio_wan@live.ca>
2021-04-11 04:47:18 +0000

    change seed value

--- db/seeds/06_lists.sql
+++ db/seeds/06_lists.sql
@@ -1,2 +1,2 @@
 INSERT INTO lists(user_id, widget_id, name, description)
-VALUES (2, 1, 'Best card on the planet', 'Hoping to one day own 3 copies');
+VALUES (2, 1, 'My Favorite Cards', 'Hoping to one day own 3 copies');

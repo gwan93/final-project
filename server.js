6d8d65158c3aea1810073956f328213e29f301b7
Ray Yiu <rayyiu66@gmail.com>
2021-04-11 18:10:58 -0700

    added dummy routing using express

--- server.js
+++ server.js
@@ -1,5 +1,60 @@
 const express = require("express");
 const path = require('path');
+const app = express();
+const morgan = require('morgan');
+const cookieSession = require('cookie-session');
+const port = 3006;
+
+app.use(morgan('dev'));
 
 const app = express();
-app.get("/login")
+app.post("/login", (req, res) => {
+    res.send('POSTING to login');
+}) // express
+app.get("/", (req, res) => {
+    res.send('Hello there');
+}) //we can say homepage, serve the index.js and the bundle (index)
+app.get("/user/:id", (req, res) => {
+    res.send('load a users profile')
+}); //load a users profile 
+
+
+//User/:id/collections
+
+app.get("/user/:id/collections/:id", (req, res) => {
+    res.send('This is where the user collections there items');
+}); //view a user's collections (favourites, etc.)
+
+app.post("/user/:id/collections/:id", (req, res) => {
+    res.send('adds a list to the user');
+});
+
+app.post("/user/:id/collections", (req, res) => {
+    res.send('create a list');
+}); //create a list
+
+app.delete("/user/:id/collections/:id/delete", (req, res) => {
+    res.send('Got a DELETE request at /user')
+});
+
+//WIDGETS 
+
+app.get("/widget", (req, res) => {
+    res.send('view all widgets')
+});
+
+app.get("/widget/:id", (req, res) => {
+    res.send('view a specific widget')
+});
+
+app.post("/widget/:id", (req, res) => {
+    res.send('change owner information for specific widget')
+});
+
+app.post("/widget", (req, res) => {
+    res.send('admin can make a new widget')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})

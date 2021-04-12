b82b32ea805e7d98bbd3e7cf6fdfd745304573b6
vagrant <gio_wan@live.ca>
2021-04-12 05:59:50 +0000

    create schemas, updated seeds, create query functions, wrote query function tests for development, create list_contents table

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+// load .env data into process.env
+require('dotenv').config();
+
 const express = require("express");
 const path = require('path');
 
 const app = express();
 app.get("/login")
+
+// Use node to connect to psql db
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+// const db = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'final_project'
+// });
+db.connect();
+module.exports = db;
